510843a1f8db918211b3f4dcce6e15d15bd38dc5
Do Trinh <contact@dotrinh.com>
2022-11-21 22:57:58 +0900

    demo drag & drop with normal view - viewgroup, thanks @Lars Vogel

---
+++ app/src/main/java/com/dotrinh/view_drag_drop/MainActivity.java
@@ -0,0 +1,84 @@
+package com.dotrinh.view_drag_drop;
+
+import static com.dotrinh.view_drag_drop.LogUtil.LogI;
+
+import android.content.ClipData;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.DragEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.myimage1).setOnTouchListener(new MyTouchListener());
+        findViewById(R.id.myimage2).setOnTouchListener(new MyTouchListener());
+        findViewById(R.id.myimage3).setOnTouchListener(new MyTouchListener());
+        findViewById(R.id.myimage4).setOnTouchListener(new MyTouchListener());
+
+        findViewById(R.id.topleft).setOnDragListener(new MyDragListener());
+        findViewById(R.id.topright).setOnDragListener(new MyDragListener());
+        findViewById(R.id.bottomleft).setOnDragListener(new MyDragListener());
+        findViewById(R.id.bottomright).setOnDragListener(new MyDragListener());
+
+    }
+
+    private final class MyTouchListener implements View.OnTouchListener {
+
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                ClipData data = ClipData.newPlainText("", "");
+                View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+                view.startDrag(data, shadowBuilder, view, 0);
+                view.setVisibility(View.INVISIBLE);
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    class MyDragListener implements View.OnDragListener {
+
+        Drawable enterShape = getResources().getDrawable(R.drawable.shape_droptarget);
+        Drawable normalShape = getResources().getDrawable(R.drawable.shape);
+
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            int action = event.getAction();
+            switch (event.getAction()) {
+                case DragEvent.ACTION_DRAG_STARTED:
+                    // do nothing
+                    break;
+                case DragEvent.ACTION_DRAG_ENTERED:
+                    v.setBackground(enterShape);
+                    break;
+                case DragEvent.ACTION_DRAG_EXITED:
+                    v.setBackground(normalShape);
+                    break;
+                case DragEvent.ACTION_DROP:
+                    // Dropped, reassign View to ViewGroup
+                    View view = (View) event.getLocalState();
+                    ViewGroup owner = (ViewGroup) view.getParent();
+                    owner.removeView(view);
+                    LinearLayout container = (LinearLayout) v;
+                    container.addView(view);
+                    view.setVisibility(View.VISIBLE);
+                    break;
+                case DragEvent.ACTION_DRAG_ENDED:
+                    v.setBackground(normalShape);
+                    LogI("ACTION_DRAG_ENDED");
+                    break;
+            }
+            return true;
+        }
+    }
+}
